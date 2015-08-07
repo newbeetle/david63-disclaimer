9ab65e064b492479752ea4eacb5b24df2bf71cf9
R3gi <regiprogi@gmail.com>
2015-08-07 08:53:00 +0200

    Update disclaimer.php
    Initial Czech translation. Updated copyright date to 2015.

--- david63/disclaimer/language/cs/disclaimer.php
+++ david63/disclaimer/language/cs/disclaimer.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package Disclaimer Extension
-* @copyright (c) 2014 david63
+* @copyright (c) 2015 david63, překlad R3gi
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
@@ -19,22 +19,6 @@ if (empty($lang) || !is_array($lang))
 {
 	$lang = array();
 }
-
-// DEVELOPERS PLEASE NOTE
-//
-// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
-//
-// Placeholders can now contain order information, e.g. instead of
-// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
-// translators to re-order the output of data while ensuring it remains correct
-//
-// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
-// equally where a string contains only two placeholders which are used to wrap text
-// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
-//
-// Some characters you may want to copy&paste:
-// ’ » “ ” …
-//
 
 $lang = array_merge($lang, array(
 	'DISCLAIMER'				=> 'Odmítnutí odpovědnosti',
